3abf0cd591bc39b7d9fbea6ece0893ba17db4b62
Robert Chang <cht8687@gmail.com>
2016-01-03 22:22:02 +1100

    Setup Redux

---
+++ TrackWorkNSW/app/containers/App.js
@@ -0,0 +1,60 @@
+'use strict'
+
+import React, {
+  Component,
+  View,
+  StyleSheet,
+  PropTypes
+} from 'react-native'
+import { connect } from 'react-redux/native'
+import { bindActionCreators } from 'redux';
+
+import allActions from '../actions';
+
+
+export default class App extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      tab: null
+    };
+  }
+
+  componentWillReceiveProps (props) {
+    const { application } = props;
+    this.setState({
+      tab: application.tab
+    })
+  }
+
+  render () {
+    const { tab } = this.state;
+
+    return (
+      <View style={styles.container}>
+        Hellow Native
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+App.propTypes = {
+  actions: PropTypes.object
+}
+
+export default connect(state => {
+  return {
+    application: state.application
+  }
+}, dispatch => {
+  return {
+    actions: bindActionCreators(allActions, dispatch)
+  }
+})(App)
---
+++ TrackWorkNSW/app/root.js
@@ -0,0 +1,31 @@
+'use strict'
+
+import React, {
+  Component,
+  StatusBarIOS
+} from 'react-native';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux/native';
+import logger from './middleware/logger';
+import thunk from 'redux-thunk';
+
+import App from './containers/App';
+
+const createStoreWithMW = applyMiddleware(logger, thunk)(createStore);
+const store = createStoreWithMW(reducers);
+
+export default class Root extends Component {
+
+  componentDidMount () {
+    StatusBarIOS.setHidden(true)
+  }
+
+  render () {
+    return (
+      <Provider store={store}>
+        {() => <App />}
+      </Provider>
+    )
+  }
+}
