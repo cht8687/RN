a1535a622124fd27ce67c6f37d737ac0dafbe178
Robert Chang <cht8687@gmail.com>
2016-01-04 18:55:42 +1100

    remove es6-enum

--- TrackWorkNSW/app/constant/application.js
+++ TrackWorkNSW/app/constant/application.js
@@ -1,9 +1,1 @@
-'use strict'
-
-import Enum from 'es6-enum'
-
-export const APP = Enum('TAB')
-
-export const GAME = Enum('INFO', 'DETAIL', 'STANDING')
-
-export const PLAYER = Enum('LIST', 'DETAIL', 'LOG', 'RECENT')
+export const TAB = 'tab';
