1cf0d6073c44981b259a0d160172711600d4dd6c
Robert Chang <cht8687@gmail.com>
2016-02-02 13:05:24 +1100

    Remove redundent code

--- TrackWorkNSW/app/utils/createReducer.js
+++ TrackWorkNSW/app/utils/createReducer.js
@@ -1,14 +1,5 @@
 'use strict'
 
-// export default function createReducer (initialState, actionHandlers) {
-//   return (state = initialState, action) => {
-//     const reduceFn = actionHandlers[action.type]
-//     if (!reduceFn) return state
-//     // Looks it works like Object.assign
-//     return { ...state, ...reduceFn(state, action) }
-//   }
-// }
-
 export default function createReducer(initialState, handlers) {
   return function reducer(state = initialState, action) {
     if (handlers.hasOwnProperty(action.type)) {
