7200311d15b304beb311249acec4ca90be6d91fa
Robert Chang <cht8687@gmail.com>
2016-02-06 01:57:47 +1100

    Add in ListView

--- TrackWorkNSW/app/components/Live.js
+++ TrackWorkNSW/app/components/Live.js
@@ -7,13 +7,21 @@ import React, {
   Text,
   TabBarIOS,
   TouchableHighlight,
-  PropTypes
+  PropTypes,
+  ListView
 } from 'react-native';
 
 export default class Live extends Component {
 
   constructor(props) {
     super(props);
+
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}); 
+    const { newsData } = this.props;
+
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2'])
+    };
   }
 
   componentWillMount() {
@@ -21,20 +29,20 @@ export default class Live extends Component {
     actions.fetchNews();
   }
 
+  _renderRow(rowData) {
+    const a = 1;
+    return <Text>{rowData}</Text>
+  }
+
   render() {
-    const { newsData } = this.props;
+
     return (
       <View style={[styles.tabContent]}>
         <Text style={styles.tabText}>Live news</Text>
-        {
-          newsData.map((e, index) => (
-       
-            <Text key={index}>
-              {e.lineName}
-            </Text>
-      
-          ))
-        }
+          <ListView
+            dataSource={this.state.dataSource} 
+            renderRow={(rowData) => <Text>{rowData}</Text>}
+          />
       </View>
     )
   }
